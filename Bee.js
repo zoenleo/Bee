f0c929c428d398e06610d38d9e13ae07445d82f3
LZhong <lz271253196@qq.com>
2017-11-15 16:34:14 +0800

    初始化

--- Bee.js
+++ Bee.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
     var root = (typeof self == 'object' && self.self == self && self) ||
         (typeof global == 'object' && global.global == global && global) ||
@@ -8,7 +8,7 @@
 
     var push = ArrayProto.push;
 
-    var _ = function(obj) {
+    var _ = function (obj) {
         if (obj instanceof _) return obj;
         if (!(this instanceof _)) return new _(obj);
         this._wrapped = obj;
@@ -27,12 +27,12 @@
 
     var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
 
-    var isArrayLike = function(collection) {
+    var isArrayLike = function (collection) {
         var length = collection.length;
         return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
     };
 
-    _.each = function(obj, callback) {
+    _.each = function (obj, callback) {
         var length, i = 0;
 
         if (isArrayLike(obj)) {
@@ -53,11 +53,11 @@
         return obj;
     }
 
-    _.isFunction = function(obj) {
+    _.isFunction = function (obj) {
         return typeof obj == 'function' || false;
     };
 
-    _.functions = function(obj) {
+    _.functions = function (obj) {
         var names = [];
         for (var key in obj) {
             if (_.isFunction(obj[key])) names.push(key);
@@ -68,14 +68,14 @@
     /**
      * 在 _.mixin(_) 前添加自己定义的方法
      */
-    _.reverse = function(string){
+    _.reverse = function (string) {
         return string.split('').reverse().join('');
     }
 
-    _.mixin = function(obj) {
-        _.each(_.functions(obj), function(name) {
+    _.mixin = function (obj) {
+        _.each(_.functions(obj), function (name) {
             var func = _[name] = obj[name];
-            _.prototype[name] = function() {
+            _.prototype[name] = function () {
                 var args = [this._wrapped];
 
                 push.apply(args, arguments);
@@ -89,8 +89,3 @@
     _.mixin(_);
 
 })()
-
-作者：冴羽
-链接：https://juejin.im/post/5a0bae515188252964213855
-来源：掘金
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
